ef33662ad4aa8cba1828b01b62c1d632299134bd
Eric Chang <efrochang@gmail.com>
2018-05-29 22:24:45 -0500

    Custom Observable and Observer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -13,15 +14,54 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() 
   {
-    const myNumbers = Observable.interval( 1000 );      // Emits every second
+    // const myNumbers = Observable.interval( 1000 );      // Emits every second
 
-    myNumbers.subscribe
-    (
-      ( myNumber: number ) =>
-      {
-        console.log( myNumber );
-      }
-    );
+    // myNumbers.subscribe
+    // (
+    //   ( myNumber: number ) =>
+    //   {
+    //     console.log( myNumber );
+    //   }
+    // );
+
+    // ----------------------
+    // Custom Observable:
+    // ----------------------
+    const myObservable = 
+      Observable.create
+      (
+        ( myObserver: Observer<string> ) =>
+        {
+          // Emit 3 times.
+
+          setTimeout
+          (
+            () => myObserver.next( "First Package" )        // Eit the 1st package after 2 secs
+            ,2000 
+          );
+
+          setTimeout
+          (
+            () => myObserver.next( "Second Package" )       // Emit the 2nd package after 4 secs
+            ,4000 
+          );          
+
+          setTimeout
+          (
+            // () => myObserver.error( "Failed!!!" )        // Emit error after 5 secs
+            () => myObserver.complete()                     // Emit complete after 5 secs
+            ,5000 
+          );          
+        }
+      );   
+
+      // Observer
+      myObservable.subscribe
+      (
+        ( data: string ) => { console.log( data ); },       // [KEY]: Called when success
+        ( error: string ) => { console.log( error ); },     // [KEY]: Called when error
+        () => { console.log( "Completed!" ); }              // [KEY]: Called when completed
+      );
   }
 
 }
